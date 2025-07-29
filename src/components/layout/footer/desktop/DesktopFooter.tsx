81d3b36cd504ea159fcd9a7c6c2b8f9f38c47ee7
batya002 <izzatulloabdusattarov@gmail.com>
2025-07-29 16:31:49 +0500

    current state [16:31]

--- src/components/layout/footer/desktop/DesktopFooter.tsx
+++ src/components/layout/footer/desktop/DesktopFooter.tsx
@@ -1,19 +1,23 @@
+import { useTranslation } from "react-i18next";
+
 export default function DesktopFooter() {
+  const { t } = useTranslation();
+
   return (
     <footer className="bg-[#212032] pt-12">
       <div className="container">
         <div className="flex items-center justify-between pb-6 border-b border-b-[#46464d]">
           <div className="w-[26rem]">
             <p className="font-bold text-3xl text-white">
-              Получите консультацию
+              {t("footer.getConsultation")}
             </p>
             <span className="font-normal text-2xl text-[#8d8d92]">
-              по телефону или в мессенджерах
+              {t("footer.byPhoneOrMessengers")}
             </span>
           </div>
           <div>
             <a
-              href="tel:+998(90)189-94-26"
+              href="tel:+998901899426"
               className="font-normal text-sm text-white block mb-2.5"
             >
               +998 (90) 189-94-26
@@ -54,31 +58,51 @@ export default function DesktopFooter() {
         </div>
         <div className="pt-8 pb-9 border-b border-b-[#46464d] flex items-center gap-x-[5.625rem]">
           <div>
-            <h3 className="font-bold text-[1rem] leading-6 text-white mb-4">Магазин</h3>
-            <a href="#" className="block font-normal text-[1rem] leading-6 text-white mb-3">Доставка</a>
-            <a href="#" className="block font-normal text-[1rem] leading-6 text-white mb-3">Оплата</a>
-            <a href="#" className="block font-normal text-[1rem] leading-6 text-white">Обмен и возврат</a>
+            <h3 className="font-bold text-[1rem] leading-6 text-white mb-4">
+              {t("footer.store")}
+            </h3>
+            <a href="#" className="block font-normal text-[1rem] leading-6 text-white mb-3">
+              {t("footer.delivery")}
+            </a>
+            <a href="#" className="block font-normal text-[1rem] leading-6 text-white mb-3">
+              {t("footer.payment")}
+            </a>
+            <a href="#" className="block font-normal text-[1rem] leading-6 text-white">
+              {t("footer.exchangeAndReturn")}
+            </a>
           </div>
           <div>
             <div className="mb-4">
-              <h3 className="font-bold text-[1rem] leading-6 text-white">Офис в Москве:</h3>
+              <h3 className="font-bold text-[1rem] leading-6 text-white">
+                {t("footer.officeMoscow")}
+              </h3>
               <div className="flex items-center gap-x-2">
                 <img src="./icons/map-pin.svg" alt="map pin icon" className="w-[0.8125rem] h-[0.8125rem]" />
-                <p className="font-normal text-[1rem] leading-6 text-white">ул. Преснеская Набережная, д. 12</p>
+                <p className="font-normal text-[1rem] leading-6 text-white">
+                  {t("footer.officeAddress")}
+                </p>
               </div>
             </div>
             <div>
-              <h3 className="font-bold text-[1rem] leading-6 text-white">Офис в Ташкенте:</h3>
+              <h3 className="font-bold text-[1rem] leading-6 text-white">
+                {t("footer.officeTashkent")}
+              </h3>
               <div className="flex items-center gap-x-2">
                 <img src="./icons/map-pin.svg" alt="map pin icon" className="w-[0.8125rem] h-[0.8125rem]" />
-                <p className="font-normal text-[1rem] leading-6 text-white">ул. Преснеская Набережная, д. 12</p>
+                <p className="font-normal text-[1rem] leading-6 text-white">
+                  {t("footer.officeAddress")}
+                </p>
               </div>
             </div>
           </div>
         </div>
         <div className="pt-9 pb-6 flex items-center justify-between">
-          <p className="font-normal text-[1rem] text-white">© 2024 Copyright</p>
-          <p className="font-normal text-[1rem] leading-6 text-white">PRESTIGERFILTR</p>
+          <p className="font-normal text-[1rem] text-white">
+            © 2024 {t("footer.copyright")}
+          </p>
+          <p className="font-normal text-[1rem] leading-6 text-white">
+            PRESTIGERFILTR
+          </p>
         </div>
       </div>
     </footer>
